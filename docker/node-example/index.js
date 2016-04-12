f81365a52b75d96c87c32891c1857e30efc66626
keke <zhoukekestar@163.com>
2016-04-12 11:14:11 +0800

    Added node-example.

---
+++ docker/node-example/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+// Constants
+var PORT = 8080;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log(__dirname)
+console.log('Running on http://localhost:' + PORT);
