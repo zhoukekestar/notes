309dac06a9a6dcb9dd5a928558b19dbee86d175e
keke <zhoukekestar@163.com>
2016-07-07 13:08:09 +0800

    Added debug

---
+++ NODEJS/README.js
@@ -0,0 +1,8 @@
+## DEBUG
+* [nodejs v6.x](https://github.com/nodejs/node/pull/6792)
+```js
+$ ./node --inspect benchmark/http_simple.js
+Debugger listening on port 5858. To start debugging, open following URL in Chrome:
+
+chrome-devtools://devtools/remote/serve_file/@4604d24a75168768584760ba56d175507941852f/inspector.html?experiments=true&v8only=true&ws=localhost:5858/node
+```
